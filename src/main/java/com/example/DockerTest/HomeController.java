e40871e3e26bf0ef619bc84afb50eb6d0a931751
Jwhyee <kawnsdud@naver.com>
2022-09-16 09:35:33 +0900

    [CREATE] - Create project

---
+++ src/main/java/com/example/DockerTest/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.DockerTest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String showMain() {
+        return "<h1>HelloWorld</h1>";
+    }
+}
